23eb3fec3cbbcdc603ca39b082b388aeaea799cb
Miroslav Purkrabek <mira.purkrabek@gmail.com>
2020-03-29 12:12:27 +0200

    repaired learning for resnet - not learning by batch loss; results visualized by PCA and LDA, LDA seems very, very good

--- train_siamese_yolo.py
+++ train_siamese_yolo.py
@@ -26,11 +26,23 @@ from torchvision import transforms
 from torch.autograd import Variable
 import torch.optim as optim
 
-EPOCHS = 1000
-BATCH = 20
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
+
+EPOCHS = 200
+BATCH = 1
+NUM_TRAIN_PLAYERS = 5
+NUM_TEST_PLAYERS = 5
+TRAIN = 0.07        # Percentage of training set (including validation)
+USED = 0.7       # Percentage of unused images (to avoid CUDA-out-of-memory error)
 MODEL_DEF = "config/yolov3-siamese.cfg"
-SIZE = 200
+SIZE = 224
 LR = 1e-4
+
+TRAIN_PLAYERS = random.sample(range(5), NUM_TRAIN_PLAYERS)
+TEST_PLAYERS = random.sample(range(5), NUM_TEST_PLAYERS)
 
 def load_dataset():
     data_path = 'data/players/'
@@ -60,26 +72,26 @@ def testModel(model, p1_img, p2_img):
 
     print("p1\\p2\t", end="")
     for p2_i in range(p2_num):
-        print("{:02d}\t".format(p2_i), end="")
+        print("{:02d}\t".format(p2_i+1), end="")
     print("\n--------------------------------------------------------------------------------------------------------------------------")
 
 
     for p1_i in range(p1_num):
-        print("{:02d}|\t".format(p1_i), end="")
+        print("{:02d}|\t".format(p1_i+1), end="")
         for p2_i in range(p2_num):
-            print("{:6.2f}\t".format(torch.dist(p1[p1_i, ...].view(1, vectorLen), p2[p2_i, ...].view(1, vectorLen))), end="")
+            print("{:6.2f}\t".format(torch.dist(p1[p1_i, ...].view(1, vectorLen), p2[p2_i, ...].view(1, vectorLen)).item()), end="")
         print()
     print("--------------------------------------------------------------------------------------------------------------------------")
     for p1_i in range(p1_num):
-        print("{:02d}|\t".format(p1_i), end="")
+        print("{:02d}|\t".format(p1_i+1), end="")
         for p1_j in range(p1_i):
-            print("{:6.2f}\t".format(torch.dist(p1[p1_i, ...].view(1, vectorLen), p1[p1_j, ...].view(1, vectorLen))), end="")
+            print("{:6.2f}\t".format(torch.dist(p1[p1_i, ...].view(1, vectorLen), p1[p1_j, ...].view(1, vectorLen)).item()), end="")
         print()
     print("--------------------------------------------------------------------------------------------------------------------------")
     for p2_i in range(p2_num):
-        print("{:02d}|\t".format(p2_i), end="")
+        print("{:02d}|\t".format(p2_i+1), end="")
         for p2_j in range(p2_i):
-            print("{:6.2f}\t".format(torch.dist(p2[p2_i, ...].view(1, vectorLen), p2[p2_j, ...].view(1, vectorLen))), end="")
+            print("{:6.2f}\t".format(torch.dist(p2[p2_i, ...].view(1, vectorLen), p2[p2_j, ...].view(1, vectorLen)).item()), end="")
         print()
 
 def computeClassDist(p1, p2):
@@ -108,27 +120,78 @@ def testmodelShort(model, players):
         # Compute distance in classes
 
     # Compute distances between classes
+    print("=======================================") 
     for p_i in range(len(p_enc)):
         md, avg, mx = computeClassDist(p_enc[p_i], p_enc[p_i])
-        print("Inter class ({:d}):\n\t[{:0.2f}, {:0.2f}, {:0.2f}]".format(p_i, md, avg, mx))
+        print("Inter class ({:d}):\n\t[{:0.2f}, {:0.2f}, {:0.2f}]".format(p_i+1, md, avg, mx))
         for p_j in range(p_i+1, len(p_enc)):
             md, avg, mx = computeClassDist(p_enc[p_i], p_enc[p_j])
-            print("{:d} vs {:d}:\n\t[{:0.2f}, {:0.2f}, {:0.2f}]".format(p_i, p_j, md, avg, mx))
+            print("{:d} vs {:d}:\n\t[{:0.2f}, {:0.2f}, {:0.2f}]".format(p_i+1, p_j+1, md, avg, mx))
         print("=======================================") 
 
+def visuzalizePCA(model, players, fname):
+    markers = [".", "+", "x", "*", "1", "s", "v"]
+    leg = ["Player 1", "Player 2", "Player 3", "Player 4", "Player 5", "Player 6", "Player 7", "Player 8"]
+    pca = PCA(n_components=2)
+    plt.clf()
+    for i, p in enumerate(players):
+        reduced = pca.fit_transform(model(p).cpu().detach())       
+        t = reduced.transpose()
+        plt.scatter(t[0], t[1], marker=markers[i])
+    plt.legend(leg[:len(players)])
+    plt.savefig(fname, format="jpg")
+    print("PCA '{}' done!".format(fname))
+
+def visuzalizeLDA(model, players, fname):
+    markers = [".", "+", "x", "*", "1", "s", "v"]
+    leg = ["Player 1", "Player 2", "Player 3", "Player 4", "Player 5", "Player 6", "Player 7", "Player 8"]
+    
+    vectors = []
+    num = [p.size(0) for p in players]
+    X = torch.cat([model(p).cpu().detach() for p in players]).numpy()
+    
+    y = []
+    for i, n in enumerate(num):
+        y += [i] * n
+    y = np.array(y)
+    # print("gt:", y)
+
+    lda = LinearDiscriminantAnalysis(n_components=2)
+    new_X = lda.fit_transform(X, y)
+    plt.clf()
+    if len(players) > 2:
+        s = 0
+        for i, n in enumerate(num):
+            plt.scatter(new_X[s:(s+n), 0], new_X[s:(s+n), 1], marker=markers[i])
+            s += n
+    else:
+        s = 0
+        for i, n in enumerate(num):
+            plt.scatter(new_X[s:(s+n)], new_X[s:(s+n)], marker=markers[i])
+            s += n
+        
+    plt.legend(leg[:len(players)])
+    plt.savefig(fname, format="jpg")
+
+    
+    print("LDA '{}' done!".format(fname))
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     os.makedirs("output", exist_ok=True)
     os.makedirs("checkpoints", exist_ok=True)
+    for path in glob.glob("PCA_*.jpg"):
+        os.remove(path)
+    for path in glob.glob("LDA_*.jpg"):
+        os.remove(path)
 
     # Initiate model
     yolo = Darknet(MODEL_DEF).to(device)
     yolo.apply(weights_init_normal)
 
     resnet = torch.hub.load('pytorch/vision:v0.5.0', 'resnet18', pretrained=True)
-    resnet.to('cuda')
+    resnet.to(device)
     loss_fcn = torch.nn.TripletMarginLoss(margin=20, p=2.0, eps=1e-02, swap=False, size_average=None, reduce=None, reduction='none')
 
     model = resnet
@@ -139,27 +202,35 @@ if __name__ == "__main__":
     print("==============================")
 
     players = []
+    test_players = []
     indices = []
-    for i in range(1, 6):
-        file_list = glob.glob("data/players/p{:d}/*.png".format(i))
-        size = (SIZE, SIZE)
-        player = torch.from_numpy(np.array([np.array(ImageOps.fit(Image.open(fname), size, Image.ANTIALIAS)) for fname in file_list])).type(torch.cuda.FloatTensor)
-        # tmp_arr = []
-        # for fname in file_list:
-        #     img = Image.open(fname)
-        #     print("original img shape", img.size)
-        #     img = ImageOps.fit(img, size, Image.ANTIALIAS)
-        #     print("Img shape", img.size)
-        #     img = np.array(img)
-        #     print("Img size", img.size)
-        #     tmp_arr.append(img)
-        # tmp_arr = np.array(tmp_arr)
-        # print(tmp_arr.size)
-        # player = torch.from_numpy(tmp_arr).type(torch.cuda.FloatTensor)
-        player = player.permute(0, 3, 1 ,2)
-        print("Player {:d} size: {}".format(i, player.size()))
-        players.append(player)
-        indices.append(list(range(player.size(0))))
+    
+    preprocess = preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(SIZE),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    for i in range(5):
+        file_list = glob.glob("data/players/p{:d}/*.png".format(i+1))
+        player = torch.stack([preprocess(Image.open(fname)) for fname in file_list]).type(torch.cuda.FloatTensor)
+        num_used = int(player.size(0) * USED)
+        player = player[:num_used, ..., ..., ...]
+        num_images = player.size(0)
+        num_train_set = int(TRAIN * num_images)
+        idx = torch.zeros(num_images, dtype=torch.bool)
+        idx[random.sample(range(num_images), num_train_set)] = True
+        train = player[idx, ..., ..., ...]
+        test = player[~idx, ..., ..., ...]
+        print("Player {:d}".format(i+1))
+        if i in TRAIN_PLAYERS:
+            print("\ttrain size: {}".format(train.size()))
+            players.append(train)
+            indices.append(list(range(train.size(0))))
+        if i in TEST_PLAYERS:
+            print("\ttest size: {}".format(test.size()))
+            test_players.append(test)
 
     optimizer = torch.optim.Adam(model.parameters())
 
@@ -182,20 +253,19 @@ if __name__ == "__main__":
             # print("target size:", target.size())
             
             idx, idx_neg = random.sample(range(len(players)), 2)
+            # idx, idx_neg = random.sample(range(2), 2)
             a, p = random.sample(range(len(indices[idx])), 2)
-            n = random.sample(range(len(indices[idx_neg])), 1)
+            n = random.sample(range(len(indices[idx_neg])), 1)[0]
             
             # print("idx: [{}, {}]\na: {}, p: {}, n: {}".format(idx, idx_neg, a, p, n))
 
-            anch = players[idx][a, ..., ..., ...].view(1, 3, SIZE, SIZE)
-            pos = players[idx][p, ..., ..., ...].view(1, 3, SIZE, SIZE)
-            neg = players[idx_neg][n, ..., ..., ...].view(1, 3, SIZE, SIZE)
+            anch = players[idx][a, ..., ..., ...].view(1, 3, SIZE, SIZE).to(device)
+            pos = players[idx][p, ..., ..., ...].view(1, 3, SIZE, SIZE).to(device)
+            neg = players[idx_neg][n, ..., ..., ...].view(1, 3, SIZE, SIZE).to(device)
                 
-            # print("Anchor size:", anch.size())
             imgs = torch.cat([anch, pos, neg], 0)
 
             imgs = Variable(imgs.to(device))
-            # print("Imgs size:", imgs.size())
         
             if model == yolo:
                 loss, outputs = model(imgs, targets=0)
@@ -203,10 +273,13 @@ if __name__ == "__main__":
                 positive = outputs[1, ...].view(1, outputs.size(1))
                 negative = outputs[2, ...].view(1, outputs.size(1))
             else:
-                outputs = model(imgs)
-                anchor = outputs[0, ...].view(1, outputs.size(1))
-                positive = outputs[1, ...].view(1, outputs.size(1))
-                negative = outputs[2, ...].view(1, outputs.size(1))
+                # outputs = model(imgs)
+                anchor = model(anch)
+                positive = model(pos)
+                negative = model(neg)
+                # anchor = outputs[0, ...].view(1, outputs.size(1))
+                # positive = outputs[1, ...].view(1, outputs.size(1))
+                # negative = outputs[2, ...].view(1, outputs.size(1))
                 loss = loss_fcn(anchor, positive, negative)
             
             loss.backward()
@@ -227,10 +300,9 @@ if __name__ == "__main__":
             # print("---")
             # dist_ap = torch.dist(anchor, positive).item()
             # dist_an = torch.dist(anchor, negative).item()
-            # dist_pn = torch.dist(positive, negative).item()
-            # # print("|\tidx: {:d}, a: {:d}, p: {:d}, n: {:d}".format(idx, a, p, n))
-            # print("|\tD(a,p): {:.1f}, D(a,n): {:.1f}, loss: {:.4f}".format(dist_ap, dist_an, loss.item()))
-            # # print("|\tD(a,p) - D(a,n) = {:.1f}".format(dist_ap-dist_an))
+            # print("|\tidx: {:03d} --> a: {:03d}, p: {:03d}".format(idx, a, p))
+            # print("|\tneg: {:03d} -->         n: {:03d}".format(idx_neg, n))
+            # print("|\tD(a,p): {:4.1f}, D(a,n): {:4.1f}, loss: {:7.4f}".format(dist_ap, dist_an, loss.item()))
             # print("---")
 
             # if (batch_i+1) % BATCH == 0:
@@ -242,30 +314,59 @@ if __name__ == "__main__":
         optimizer.step()
         optimizer.zero_grad()
 
-        epoch_time = datetime.timedelta(seconds=time.time() - epoch_time)
-        sum_loss += loss.item()
         # print("\r\tloss {:.4f}, time: {}".format(loss.item(), epoch_time), end="")
         # print("Epoch {:d}".format(epoch))
 
+        # print("Epoch {:d}\r".format(epoch), end="")
+        visuzalizeLDA(model, test_players, "LDA_epoch_{:03d}.jpg".format(epoch))
+        # visuzalizePCA(model, players, "PCA_epoch_{:03d}.jpg".format(epoch))
 
         if (epoch+1) % 20 == 0:
-            # print("\nEpoch {:d}:".format(epoch+1))
             # print("================================================= vv epoch {:d} vv ===============================================================".format(epoch))
             # testModel(model, player1[..., ..., ..., ...], player2[..., ..., ..., ...])
             # print("================================================= ^^ epoch {:d} ^^ ===============================================================".format(epoch))
             torch.save(model.state_dict(), "checkpoints/yolov3_id_ckpt_%d.pth" % epoch)
         if (epoch) % 50 == 0:
+            # print("---")
+            # dist_ap = torch.dist(anchor, positive).item()
+            # dist_an = torch.dist(anchor, negative).item()
+            # print("|\tidx: {:03d} --> a: {:03d}, p: {:03d}".format(idx, a, p))
+            # print("|\tneg: {:03d} -->         n: {:03d}".format(idx_neg, n))
+            # print("|\tD(a,p): {:4.1f}, D(a,n): {:4.1f}, loss: {:7.4f}".format(dist_ap, dist_an, loss.item()))
+            # print("---")
             print("================================================= vv epoch {:d} vv ===============================================================".format(epoch))
             idx1, idx2 = [0, 1]
+            num_players = min(8, min(len(indices[idx1]), len(indices[idx2])))
             testModel(model,
-                players[idx1][random.sample(range(len(indices[idx1])), 8), ..., ..., ...],
-                players[idx2][random.sample(range(len(indices[idx2])), 8), ..., ..., ...]
+                players[idx1][random.sample(range(len(indices[idx1])), num_players), ..., ..., ...],
+                players[idx2][random.sample(range(len(indices[idx2])), num_players), ..., ..., ...]
+                # players[idx1][:num_players, ..., ..., ...],
+                # players[idx2][:num_players, ..., ..., ...]
             )
             testmodelShort(model, players)
+            visuzalizeLDA(model, players, "LDA_epoch_{:03d}.jpg".format(epoch))
+            # visuzalizePCA(model, players, "PCA_epoch_{:03d}.jpg".format(epoch))
+            
+            epoch_time = datetime.timedelta(seconds=time.time() - epoch_time)
+            elapsed_time = datetime.timedelta(seconds=time.time() - start_time)
+            print("Epoch time: {}".format(epoch_time))
+            print("Elapsed time: {}".format(elapsed_time))
             print("================================================= ^^ epoch {:d} ^^ ===============================================================".format(epoch))
             
     # testModel(model, player1, player2)
     
+    training_time = datetime.timedelta(seconds=time.time() - start_time)
+    print("Training time: {}".format(training_time))
+
+    testmodelShort(model, players)
+    visuzalizeLDA(model, players, "LDA_final.jpg")
+    # visuzalizePCA(model, players, "PCA_final.jpg")
+    
+    print("================================================= Test set ===============================================================")
+    testmodelShort(model, test_players)
+    visuzalizeLDA(model, test_players, "LDA_test_set.jpg")
+    # visuzalizePCA(model, test_players, "PCA_test_set.jpg")
+    print("================================================= Test set ===============================================================")
+    
     overall_time = datetime.timedelta(seconds=time.time() - start_time)
     print("Overall time: {}".format(overall_time))
-
